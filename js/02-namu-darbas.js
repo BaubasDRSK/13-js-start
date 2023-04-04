379c6556a69c0c76117605db0e6288a4a852ecac
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-04-04 19:57:55 +0300

    nd 02

---
+++ js/02-namu-darbas.js
@@ -0,0 +1,22 @@
+function tusciaFunkcija(){
+    return 'false';
+    
+}
+
+console.log(tusciaFunkcija());
+
+// DAUGYBA
+const skaicius1 = 20;
+const skaicius2 = 3.14;
+const skaicius3 = -2;
+
+function daugyba(sk1, sk2 ){
+    const sandauga = sk1 * sk2;
+    return sandauga;
+}
+
+console.log(daugyba(skaicius1, skaicius2));
+console.log(daugyba(skaicius3, skaicius2));
+console.log(daugyba(skaicius1, skaicius3));
+
+//skaitmenu kiekis skaiciuje    
